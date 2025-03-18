7ca1cd2ba1675e9a3377541e9b8c680596251495
iheb.benayed <iheb.benayed@edifixio.com>
2025-03-18 13:41:41 +0100

    add vpc endpoint

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,3 +7,11 @@ resource "aws_subnet" "private_subnet" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
 }
+
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id       = aws_vpc.my_vpc.id
+  service_name = "com.amazonaws.us-east-1.dynamodb"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [aws_vpc.my_vpc.default_route_table_id]
+}
