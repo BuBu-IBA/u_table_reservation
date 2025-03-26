5facb34985be88c2a1f44e6091bebc2ac1912145
iheb.benayed <iheb.benayed@edifixio.com>
2025-03-26 11:55:47 +0100

    Specify semantic versions

--- provider.tf
+++ provider.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.91.0"
+      version = "~> 5.91.0"
     }
   }
+  required_version = "~> 1.11.0"
 }
 provider "aws" {
   region = var.region
